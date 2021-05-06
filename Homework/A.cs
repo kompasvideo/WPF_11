0450ed145db15383aff885c20215bfdeb3f7a6d4
Барышников андрей <andrey1andrey@narod.ru>
2021-05-06 23:27:25 +0300

    1

---
+++ Homework/A.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    interface I1
+    {
+        void M();
+    }
+
+    interface I2
+    {
+        void M();
+    }
+
+    class A
+    {
+        public void M() { Console.WriteLine("A.M()"); } 
+    }
+
+    class B:A
+    {
+
+    }
+}
